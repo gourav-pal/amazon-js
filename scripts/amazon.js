6d956589aa26b72905327122f5b256bd616bf3c4
gourav-pal <your.email@example.com>
2024-06-25 01:28:52 +0530

    adding using select dropdown

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -27,7 +27,7 @@ products.forEach((product)=>{
           </div>
 
           <div class="product-quantity-container">
-            <select>
+            <select class="js-quantity-selector-${product.id}">
               <option selected value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -58,10 +58,18 @@ products.forEach((product)=>{
 
 document.querySelector('.js-products-grid').innerHTML=productsHTML;
 
+
+
 document.querySelectorAll('.js-add-to-cart')
   .forEach((button)=>{
     button.addEventListener('click',()=>{
       const productId=button.dataset.productId;
+      
+      const quantitySelector = document.querySelector(
+        `.js-quantity-selector-${productId}`
+      );
+      const quantity = Number(quantitySelector.value);
+      
       let matchItem='';
       cart.forEach((item)=>{
         if(item.productId===productId){
@@ -73,7 +81,7 @@ document.querySelectorAll('.js-add-to-cart')
       }else{
         cart.push({
           productId:productId,
-          quantity:1
+          quantity:quantity
         })
       }
       
@@ -84,6 +92,10 @@ document.querySelectorAll('.js-add-to-cart')
       })
 
       document.querySelector('.js-cart-quantity').innerHTML=cartQuantity;
+
+      
+      
+      
 
     });
   });
